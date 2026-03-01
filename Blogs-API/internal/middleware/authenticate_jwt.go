c3c93b92e09ba3536130a780081cbe9e786858f8
Divine Ositadinma <akindivine587@gmail.com>
2026-03-01 17:02:28 +0100

    middleware update

--- Blogs-API/internal/middleware/authenticate_jwt.go
+++ Blogs-API/internal/middleware/authenticate_jwt.go
@@ -1,7 +1,50 @@
 package middleware
 
-import "net/http"
+import (
+	m "blog/pkg/models"
+	h "blog/pkg/utils"
+	"context"
+	"fmt"
+	"net/http"
+	"os"
+	"strings"
 
-func AuthenticateJWT(next http.Handler) {
-	return
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthenticateJWT(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		clientToken := strings.Split(r.Header.Get("authorization"), " ")
+
+		if len(clientToken) != 2 {
+			errorMessage := h.ErrorMessageJson(h.UNAUTHORIZED_ACCESS, h.UNAUTHORIZED_ACCESS_CODE, h.UNAUTHORIZED_ACCESS_DETAIL)
+			h.Response(w, r, errorMessage, http.StatusUnauthorized)
+			return
+		}
+
+		token := clientToken[1]
+
+		claims := &m.SignedUser{}
+
+		jwtToken, err2 := jwt.ParseWithClaims(token, claims, func(t *jwt.Token) (any, error) {
+			if t.Method != jwt.SigningMethodHS256 {
+				return nil, fmt.Errorf("Method mismatch.")
+			}
+
+			return os.Getenv("JWT_SECRET_KEY"), nil
+		})
+		if err2 != nil {
+			errorMessage := h.ErrorMessageJson(h.UNAUTHORIZED_ACCESS, h.UNAUTHORIZED_ACCESS_CODE, h.UNAUTHORIZED_ACCESS_DETAIL)
+			h.Response(w, r, errorMessage, http.StatusUnauthorized)
+			return
+		}
+
+		if !jwtToken.Valid {
+			errorMessage := h.ErrorMessageJson(h.UNAUTHORIZED_ACCESS, h.UNAUTHORIZED_ACCESS_CODE, h.UNAUTHORIZED_ACCESS_DETAIL)
+			h.Response(w, r, errorMessage, http.StatusUnauthorized)
+			return
+		}
+		ctx := context.WithValue(r.Context(), "user_id", claims.ID)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 }
